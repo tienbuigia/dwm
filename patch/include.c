ed33b528764d566f80cac61ddb32ac30e3b253bb
tien <tien.bg187345@sis.hust.edu.vn>
2022-07-12 12:21:48 +0700

    finalized from flexipatch and will be maintain this way from now on

---
+++ patch/include.c
@@ -0,0 +1,33 @@
+/* Bar functionality */
+#include "bar_indicators.c"
+#include "bar_tagicons.c"
+#include "bar.c"
+
+#include "bar_dwmblocks.c"
+#include "bar_ltsymbol.c"
+#include "bar_status.c"
+#include "bar_status2d.c"
+#include "bar_statuscmd.c"
+#include "bar_tags.c"
+#include "bar_wintitle.c"
+#include "bar_systray.c"
+
+/* Other patches */
+#include "attachx.c"
+#include "cfacts.c"
+#include "restartsig.c"
+#include "scratchpad.c"
+#include "shift.c"
+#include "shiftviewclients.c"
+#include "swallow.c"
+#include "togglefullscreen.c"
+#include "vanitygaps.c"
+#include "xrdb.c"
+#include "seamless_restart.c"
+/* Layouts */
+#include "layout_facts.c"
+#include "layout_centeredmaster.c"
+#include "layout_centeredfloatingmaster.c"
+#include "layout_monocle.c"
+#include "layout_tile.c"
+
---
+++ patch/include.h
@@ -0,0 +1,32 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_dwmblocks.h"
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_status2d.h"
+#include "bar_statuscmd.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_systray.h"
+
+/* Other patches */
+#include "attachx.h"
+#include "cfacts.h"
+#include "restartsig.h"
+#include "scratchpad.h"
+#include "seamless_restart.h"
+#include "shift.h"
+#include "shiftviewclients.h"
+#include "swallow.h"
+#include "togglefullscreen.h"
+#include "vanitygaps.h"
+#include "xrdb.h"
+/* Layouts */
+#include "layout_centeredmaster.h"
+#include "layout_centeredfloatingmaster.h"
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
